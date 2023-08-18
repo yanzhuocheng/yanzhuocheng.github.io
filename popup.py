9fc2029756f4e776ef223049aabfe4b055e9b01d
yanzhuocheng <142620590+yanzhuocheng@users.noreply.github.com>
2023-08-19 02:20:02 +0800

    Update popup.py

--- popup.py
+++ popup.py
@@ -3,17 +3,36 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Simple Website</title>
+  <title>Complex Website</title>
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <header>
-    <h1>颜卓骋</h1>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
   </header>
+  
   <main>
-    <p>This is a basic example of a simple website.</p>
-    <button id="changeTextButton">Change Text</button>
+    <h1>Welcome to Our Complex Website</h1>
+    <p>This is an example of a more complex website with multiple pages and features.</p>
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+      <button type="submit">Submit</button>
+    </form>
   </main>
+  
+  <footer>
+    <p>&copy; 2023 Complex Website. All rights reserved.</p>
+  </footer>
+  
   <script src="script.js"></script>
 </body>
 </html>
