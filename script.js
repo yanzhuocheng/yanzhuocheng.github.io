b1e88d63b4c3a74e0517eb30129964a6fb10d3d1
yanzhuocheng <142620590+yanzhuocheng@users.noreply.github.com>
2023-08-19 02:27:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.querySelector("form");
+// 设置倒计时日期
+var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
 
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
+var countdownFunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    alert(`Thank you, ${name}! We will contact you at ${email}.`);
-  });
-});
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // 当倒计时结束时，显示一条消息
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "倒计时已结束!";
+    }
+}, 1000);
