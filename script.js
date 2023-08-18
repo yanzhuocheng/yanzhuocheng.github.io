9632c2e2b93bee25a2089312e5dbaf13e7fb6dce
yanzhuocheng <142620590+yanzhuocheng@users.noreply.github.com>
2023-08-19 02:39:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const countdownDateInput = document.getElementById("countdownDate");
-  const startButton = document.getElementById("startButton");
-  const countdownDisplay = document.getElementById("countdown");
+  const rollButton = document.getElementById("rollButton");
+  const dice = document.getElementById("dice");
 
-  startButton.addEventListener("click", function() {
-    const countdownDate = new Date(countdownDateInput.value).getTime();
-    const interval = setInterval(function() {
-      const now = new Date().getTime();
-      const timeRemaining = countdownDate - now;
-      
-      if (timeRemaining <= 0) {
-        clearInterval(interval);
-        countdownDisplay.textContent = "Countdown expired!";
-      } else {
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-        countdownDisplay.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-      }
-    }, 1000);
+  rollButton.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    dice.textContent = randomNumber;
   });
 });
